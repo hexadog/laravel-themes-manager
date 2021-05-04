d8489643349a0b767d13c9e3d843609fd2f0a096
Gaetan <gaetan@hexadog.com>
2021-05-04 21:03:38 +0200

    chore(ci): fix Invalid php-cs-fixer configuration property

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -20,7 +20,7 @@ return $config
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
         'not_operator_with_successor_space' => true,
         'trailing_comma_in_multiline' => true,
