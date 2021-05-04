626b47a3f7dc5a5cfd08ead9aa908bc04d3f51c5
Gaetan <gaetan@hexadog.com>
2021-05-04 21:00:45 +0200

    chore(ci): fix php-cs-fixer config file to new format

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,6 @@
 <?php
 
-$finder = Symfony\Component\Finder\Finder::create()
+$finder = PhpCsFixer\Finder::create()
     ->notPath('vendor/*')
     ->notPath('resources/*')
     ->notPath('database/*')
@@ -15,7 +15,8 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+return $config
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
